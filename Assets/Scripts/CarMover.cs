b1051c0478cf077a7bcd98917c9e24c37b0ae6bd
Mohamed Benkedadra <hammicristo@gmail.com>
2017-08-20 10:12:31 +0200

    adding car script

---
+++ Assets/Scripts/CarMover.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMover : MonoBehaviour {
+    public Collider[] WheelColliders = new Collider[4]; // Getting Wheel Colliders
+    public Transform[] WheelMeshes = new Transform[4]; // Getting Wheel Meshes to move them
+
+	void UpdateMeshPos() {
+
+    }
+}
