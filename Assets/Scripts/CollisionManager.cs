e63f4ce84ec5f7b5db9ebc83655668f58d72cce7
Mohamed Benkedadra <hammicristo@gmail.com>
2017-08-23 14:52:00 +0200

    added Collision Detection

---
+++ Assets/Scripts/CollisionManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionManager : MonoBehaviour {
+    public bool isColliding = false;
+    public bool colliderType = false;
+
+    void OnCollisionEnter(Collision col) {
+        isColliding = true;
+        if (col.gameObject.tag == "LoadedTerrain")
+            colliderType = false;
+        else
+            colliderType = true;
+    }
+
+    void OnCollisionExit(Collision col) {
+        isColliding = false;
+        colliderType = false;
+    }
+
+    public void CollisionDetection() {
+        if (isColliding == true) {
+            Debug.Log("Object is Colliding");
+            if (colliderType == false)
+                Debug.Log("Terrain");
+            else
+                Debug.Log("notTerrain");
+        }
+        else
+            Debug.Log("Object isn't Colliding");
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private Rigidbody Player;
+    public float limit = 5.5f;
+    public float speed = 800;
+    public float moveForward = 1, leftAndRight = 1;
+    public float upAndDown = 0.72f;
+
+    void Start () {
+        Player = GetComponent<Rigidbody>();
+	}
+
+    void FixedUpdate() {
+        CarMover(); // will move the car l&r and forward
+        MeshUpdater(); // will update car Wheels and Car body when changing lanes
+        SpeedManager(); // Manages Speed
+        gameObject.GetComponent<CollisionManager>().CollisionDetection(); // is Managing COLLISIONS
+    }
+
+    void CarMover() {
+        float moveHorizontal = Input.GetAxis("Horizontal") * leftAndRight;
+        Vector3 movement = new Vector3(moveHorizontal, upAndDown, moveForward); // get axis will smpoth left and right
+        movement.x *= speed;
+        movement.z *= speed;
+        Player.velocity = movement * Time.deltaTime; // moving the player and using dt to smooth transition 
+
+        Player.position = new Vector3(Mathf.Clamp(Player.position.x, -limit, limit), upAndDown, Player.position.z);
+        Player.transform.rotation = Quaternion.identity;
+    }
+
+    void MeshUpdater() {
+
+    }
+
+    void SpeedManager() {
+        if (gameObject.GetComponent<CollisionManager>().isColliding == true 
+            && gameObject.GetComponent<CollisionManager>().colliderType == false && speed > 800 )
+                speed -= Time.deltaTime * 200;
+        else if (speed < 2100) // will increase speed by time
+                speed += Time.deltaTime * 50;
+    } 
+}
