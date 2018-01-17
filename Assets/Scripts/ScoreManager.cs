b760f0d92aed1e004041bbaf3f119d5b4fff04c0
Mohamed Benkedadra <hammicristo@gmail.com>
2018-01-17 17:44:13 +0100

    added a score managing system

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour {
+
+	[SerializeField]
+	private int score;
+	private int scoreAddition = 100;
+	void Start () {
+		initScore();
+		
+	}
+	void OnTriggerExit(Collider AI){
+		if (AI.transform.position.z < gameObject.transform.position.z && AI.gameObject.tag == "AI")
+			 addScore(scoreAddition);
+    }
+
+	private void addScore(int value){
+		setScore(getScore() + value);
+	}
+
+	private void initScore(){
+		score = 0;
+	}
+
+	private int getScore(){
+		return score;
+	}
+
+	private void setScore(int value){
+		score = value;
+	}
+}
